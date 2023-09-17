089101a67ddb0f83c26f3fe384b42767de8d10da
Utkarsh pravind <46813085+utkarshpr@users.noreply.github.com>
2023-09-17 10:37:30 +0530

    swagger

--- learn/src/main/java/com/learn/config/SecurityConfig.java
+++ learn/src/main/java/com/learn/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.learn.service.Security.CustomUserDetailService;
 import com.learn.service.Security.JwtAuthenticationEntryPoint;
@@ -25,6 +26,7 @@ import com.learn.service.Security.JwtAuthenticationFilter;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebMvc
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -34,6 +36,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	private JwtAuthenticationFilter jwtAuthenticationFilter;
+	
+	public static final String PUBLIC_URLS[]= {
+			"/api/v1/auth/**","/v3/api-docs","/swagger-resources/**", "/swagger-ui/**", "/webjars/**","/v2/api-docs"
+			};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -43,8 +49,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.csrf()
 		.disable()
 		.authorizeHttpRequests()
-		.antMatchers("/api/v1/auth/login/").permitAll()
-		.antMatchers("/api/v1/auth/register/").permitAll()
+		.antMatchers(PUBLIC_URLS).permitAll()
 		.antMatchers(HttpMethod.GET).permitAll()
 		.anyRequest()
 		.authenticated()
